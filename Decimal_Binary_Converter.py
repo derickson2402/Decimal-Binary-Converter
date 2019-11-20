4db650b8a146d0abd59eab786f0754f627737b0a
derickson2402 <derickson2402@gmail.com>
2019-11-19 20:38:46 -0500

    Made long output values easier to read, and greatly simplified functions

--- Decimal_Binary_Converter.py
+++ Decimal_Binary_Converter.py
@@ -17,14 +17,10 @@ def Collect_Conversion_Type(conv_type_in):
     Collecting = True
     while (Collecting == True):
         Collecting = False
-        if ("1" in(conv_type_in.lower())):
-            return("BtD")
-        elif ("binary to decimal" in(conv_type_in.lower())):
-            return("BtD")
-        elif ("2" in(conv_type_in)):
-            return("DtB")
-        elif ("decimal to binary" in(conv_type_in.lower())):
-            return("DtB")
+        if (("1" in(conv_type_in.lower())) or ("binary to decimal" in(conv_type_in.lower()))):
+            return("Type Bin to Dec")
+        elif (("2" in(conv_type_in)) or ("decimal to binary" in(conv_type_in.lower()))):
+            return("Type Dec to Bin")
         elif ("no" in(conv_type_in.lower())):
             exit("\nWow, fine then I guess\n")
         else:
@@ -42,31 +38,41 @@ def Collect_Number(num_in):
         else:
             return(int(num_in))
 
-def Biggest_Multiple_of_2(num_in):
-    multiple = 0
-    while(num_in/(2**multiple) >= 1):
-        multiple += 1
-    return(multiple-1)
+def Largest_Multiple(Input, Mult_of):
+    iteration = 0
+    while(Input/(Mult_of**iteration) >= 1):
+        iteration += 1
+    return(iteration-1)
 
-def Biggest_Multiple_of_10(num_in):
-    multiple = 0
-    while(num_in/(10**multiple) >= 1):
-        multiple += 1
-    return(multiple-1)
+def Convert_Number(Conversion_Value, Convert_type):
+    Running_Sum = 0
+    while(Conversion_Value != 0) and (Convert_type == "Type Dec to Bin"):
+        Running_Sum += 10**(Largest_Multiple(Conversion_Value, 2))
+        Conversion_Value -= 2**(Largest_Multiple(Conversion_Value, 2))
+    while(Conversion_Value != 0) and (Convert_type == "Type Bin to Dec"):
+        Running_Sum += 2**(Largest_Multiple(Conversion_Value, 10))
+        Conversion_Value -= 10**(Largest_Multiple(Conversion_Value, 10))
+    return(str(Running_Sum))
 
-def Convert_Dec_to_Bin(Dec_num):
-    Running_Binary_Sum = 0
-    while(Dec_num != 0):
-        Running_Binary_Sum += 10**(Biggest_Multiple_of_2(Dec_num))
-        Dec_num -= 2**(Biggest_Multiple_of_2(Dec_num))
-    return(Running_Binary_Sum)
-
-def Convert_Bin_to_Dec(Bin_num):
-    Running_Decimal_Sum = 0
-    while(Bin_num != 0):
-        Running_Decimal_Sum += 2**(Biggest_Multiple_of_10(Bin_num))
-        Bin_num -= 10**(Biggest_Multiple_of_10(Bin_num))
-    return(Running_Decimal_Sum)
+def Make_Output_Readable(Input, Input_type):
+    Running_Output = ""
+    iteration = 0
+    length = int(len(Input))
+    if (Input_type == "Type Dec to Bin"):
+        while(length-(iteration*4) >= 4):
+            Running_Output=Input[length-4-(iteration*4):length-(iteration*4)]+(""if(iteration==0)else" ")+Running_Output
+            iteration += 1
+        if(length-(iteration*4) > 0):
+            Running_Output = "0"*(4-(length-(iteration*4))) + Input[0:length-(iteration*4)]+" "+Running_Output
+    elif (Input_type == "Type Bin to Dec"):
+        while(length-(iteration*3) >= 3):
+            Running_Output=Input[length-3-(iteration*3):length-(iteration*3)]+(""if(iteration==0)else",")+Running_Output
+            iteration+=1
+        if((length-iteration*4)>0):
+            Running_Output = Input[0:length-(iteration*3)]+","+Running_Output
+    else:
+        exit("There was an error recieving an input type for the Make_Output_Readable function")
+    return(Running_Output)
 
 print("\n"*100, "This program will take a Binary number and convert it to Decimal,")
 print("or take a Decimal number and convert it to Binary", end="\n\n")
@@ -75,13 +81,8 @@ print("Please note that only integer values are accepted", end= "\n\n")
 Program_Running = True
 while(Program_Running == True):
     Convert_type = Collect_Conversion_Type(input("Would you like to convert 1) Binary to Decimal or 2) Decimal to Binary?\n\n"))
-    print(end="\n")
-    Convert_number = Collect_Number(input("Please enter your number below:\n\n"))
-    print(end="\n")
-    if(Convert_type == "DtB"):
-        print("Your number in binary is: ", Convert_Dec_to_Bin(Convert_number), end='\n\n')
-    elif(Convert_type == "BtD"):
-        print("Your number in decimal is: ", Convert_Bin_to_Dec(Convert_number), end='\n\n')
+    Value_To_Convert = Collect_Number(input("\nPlease enter your number below:\n\n"))
+    print("\nYour new value in ",("Binary" if Convert_type == "Type Bin to Dec" else "Decimal"),"is:",Make_Output_Readable(Convert_Number(Value_To_Convert,Convert_type),Convert_type),end='\n\n')
     foo = input("Would you like to convert another number? Yes or No?\n\n")
     print(end='\n')
     if("y" in(foo.lower())):
